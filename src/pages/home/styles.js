7df2e179caf959cbd4eb552835559c8a3b56b29e
eurebecagarcia <rebecagarciarodrigues21@gmail.com>
2022-10-01 15:02:12 -0300

    ending cards and starting section

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -3,5 +3,9 @@ import styled from "styled-components/macro";
 export const BoxCard = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+  justify-content: space-around;
+  background-image: ${({ image }) => `url(${image})`};
+  background-size: cover;
+  background-position: center;
+  padding: 10px;
 `;
