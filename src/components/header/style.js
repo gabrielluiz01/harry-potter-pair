c7ac3337755fc0b9fbbbbc73cf2ad37446d54c10
gabrielluiz01 <gabriel.luizvieira01@gmail.com>
2022-10-01 13:23:58 -0300

    making header component

---
+++ src/components/header/style.js
@@ -0,0 +1,40 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.header`
+  width: 100%;
+  background: #212121;
+  padding: 0.5rem 3rem;
+  box-shadow: 1px 1px 20px 1px #fae032;
+  margin-bottom: 2.5rem;
+`;
+
+export const Menu = styled.nav`
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    color: #fae032;
+    width: 50%;
+
+    li {
+      font-size: 18px;
+      cursor: pointer;
+      transition: 0.5s all;
+
+      :hover {
+        text-decoration: underline;
+        filter: brightness(80%);
+      }
+    }
+  }
+`;
+
+export const LogoImg = styled.img`
+  width: 120px;
+  opacity: 1;
+`;
